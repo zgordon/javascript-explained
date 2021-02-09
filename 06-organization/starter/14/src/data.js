27a826420fac7c67ac7b57dd6bfa636d74fe4839
Ike Maldonado <maldonadoike@gmail.com>
2021-02-09 10:50:56 -0800

    Separated posts into own data.js file

---
+++ 06-organization/starter/14/src/data.js
@@ -0,0 +1,17 @@
+const posts = [
+    {
+        title: "Of Mice and Men",
+        description: "The best selling about two friends struggling to make ends meet."
+    },
+    {
+        title: "Grapes of Wrath",
+        description: "The something of a something describes something here."
+    },
+    {
+        title: "Harry Potter and the Order of the Phoenix",
+        description: "A book about a magical wizard discovering how he fits in a messed up family"
+    }
+];
+
+
+export default posts;
